535da71d3dcbb48a0489e35c9b6ab673eda69efc
FtobarV64 <ftobarv@gmail.com>
2022-05-26 14:59:03 -0400

    Creating Pet model, controller, views commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_26_184941) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_185745) do
   create_table "clients", force: :cascade do |t|
     t.string "phone"
     t.string "name"
@@ -20,4 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_26_184941) do
     t.index ["phone"], name: "index_clients_on_phone", unique: true
   end
 
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "race"
+    t.date "birthday"
+    t.integer "Client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["Client_id"], name: "index_pets_on_Client_id"
+  end
+
+  add_foreign_key "pets", "Clients"
 end
